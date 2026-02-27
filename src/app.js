bbea2e339b9e3aca9876dd32773c6ece09dc4a48
shafin115 <shafinjunayed954@gmail.com>
2026-02-27 22:59:40 +0600

    feat: Added Frontend and others to show the users modifications

--- src/app.js
+++ src/app.js
@@ -1,18 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
 const authRoutes = require("./routes/auth.routes");
 const userRoutes = require("./routes/user.routes");
 
+// Enable CORS for all origins
+app.use(cors());
 app.use(express.json());
 
-// Root route
 app.get("/", (req, res) => {
   res.send("Hello! Server is running...");
 });
 
-// Example inline user route
-app.use("/api/users", userRoutes);
-// Auth routes
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 module.exports = app;
