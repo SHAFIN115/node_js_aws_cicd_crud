ef8908f65e861a72c6e964ec1808607432a2c771
Shafin Junayed <shafinjunayed954@gmail.com>
2026-03-12 23:23:13 +0600

    fix: Change port to show the FE using the npm run dev

--- src/app.js
+++ src/app.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+
 const app = express();
 
 const authRoutes = require("./routes/auth.routes");
 const userRoutes = require("./routes/user.routes");
 
-// Enable CORS for all origins
+// Enable CORS
 app.use(cors());
 app.use(express.json());
 
+// Serve frontend static files
+app.use(express.static(path.join(__dirname, "../frontend")));
+
 app.get("/", (req, res) => {
-  res.send("Hello! Server is running...");
+  res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
 
+// API routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
