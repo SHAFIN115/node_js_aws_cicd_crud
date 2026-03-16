aa416d1e821f662408828af0e7120578a7863189
Shafin Junayed <shafinjunayed954@gmail.com>
2026-03-17 02:56:07 +0600

    fix: Update project directory paths in install and start scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-cd /home/ubuntu/nodeapp
+# Navigate to the project directory
+cd /home/ec2-user/node_js_aws_cicd_crud || exit 1
+
+# Install dependencies
 npm install
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-cd /home/ubuntu/nodeapp
+# Navigate to the project directory
+cd /home/ec2-user/node_js_aws_cicd_crud || exit 1
+
+# Restart or start the app with pm2
 pm2 restart server.js || pm2 start server.js
