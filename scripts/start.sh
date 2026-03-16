83682c71267c15ec490ffce116cc7e1e288bc402
Shafin Junayed <shafinjunayed954@gmail.com>
2026-03-17 03:53:16 +0600

    fix: Improve application start process by ensuring old process is stopped before starting a new one

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,8 +6,11 @@ APP_DIR="/home/ec2-user/node_js_aws_cicd_crud"
 
 cd "$APP_DIR"
 
-echo "Starting application..."
-pm2 restart node_crud_app || pm2 start npm --name node_crud_app -- run start
+echo "Stopping old process..."
+pm2 delete node_crud_app || true
+
+echo "Starting fresh..."
+pm2 start npm --name node_crud_app -- run start
 
 echo "Saving pm2 process list..."
 pm2 save
