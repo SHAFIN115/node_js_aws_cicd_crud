2ec85a682b8b087d3bcff4c6b7e87f1ffb0ff5b2
Shafin Junayed <shafinjunayed954@gmail.com>
2026-03-20 00:29:25 +0600

    feat: Add Prometheus metrics integration and update dependencies

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const { client, httpRequestCounter, httpRequestDuration } = require("./metrics");
 
 const app = express();
 
@@ -10,6 +11,24 @@ const userRoutes = require("./routes/user.routes");
 // Enable CORS
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  const end = httpRequestDuration.startTimer();
+
+  res.on("finish", () => {
+    httpRequestCounter.inc({
+      method: req.method,
+      route: req.path,
+      status: res.statusCode,
+    });
+    end({ method: req.method, route: req.path });
+  });
+
+  next();
+});
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", client.register.contentType);
+  res.end(await client.register.metrics());
+});
 
 // Serve frontend static files
 app.use(express.static(path.join(__dirname, "../frontend")));
---
+++ src/metrics.js
@@ -0,0 +1,18 @@
+const client = require('prom-client');
+
+client.collectDefaultMetrics();
+
+const httpRequestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'route', 'status'],
+});
+
+const httpRequestDuration = new client.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'HTTP request duration in seconds',
+  labelNames: ['method', 'route'],
+  buckets: [0.1, 0.3, 0.5, 1, 2, 5],
+});
+
+module.exports = { client, httpRequestCounter, httpRequestDuration };
